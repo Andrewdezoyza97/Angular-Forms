ae1a8a4e27d048420ec014521ce574ddbac9f557
Andrew De Zoyza <andrewdezoyza@aizenit.com>
2023-04-19 22:02:58 +0530

    upto forms validation

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { UserSettingFormComponent } from './user-setting-form/user-setting-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserSettingFormComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-setting-form',
+  templateUrl: './user-setting-form.component.html',
+  styleUrls: ['./user-setting-form.component.css']
+})
+export class UserSettingFormComponent implements OnInit {
+
+  originalUserSettings: UserSettings = {
+    name: '',
+    emailOffers: false,
+    interfaceStyle: '',
+    subscriptionType: '',
+    notes: ''
+  };
+
+  userSettings: UserSettings = { ...this.originalUserSettings };
+
+  constuctor() { }
+
+  ngOnInit() {
+
+  }
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+  }
+
+}
