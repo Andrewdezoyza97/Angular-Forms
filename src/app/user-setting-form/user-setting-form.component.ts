76efb22abdb7b21aea0b7c9abd7eb6c50aeafafa
Andrew De Zoyza <andrewdezoyza@aizenit.com>
2023-04-20 19:24:32 +0530

    Final

--- src/app/user-setting-form/user-setting-form.component.ts
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm } from '@angular/forms';
-import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-user-setting-form',
   templateUrl: './user-setting-form.component.html',
   styleUrls: ['./user-setting-form.component.css']
 })
-
-
 export class UserSettingFormComponent implements OnInit {
 
   originalUserSettings: UserSettings = {
@@ -21,22 +18,13 @@ export class UserSettingFormComponent implements OnInit {
   };
 
   userSettings: UserSettings = { ...this.originalUserSettings };
-  dataService: any;
 
-  constuctor( dataService : DataService) {
-
-   }
+  constuctor() { }
 
   ngOnInit() {
 
   }
   onSubmit(form: NgForm) {
-    console.log('in onSubmit: ', form.valid);
-    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
-      (      result: any) => console.log('success:', result),
-      (      error: any) => console.log('error:', error)
-
-    );
   }
 
 }
